fa7c0e34dbfc0b9ebb9b5c051799551a6e87538d
Varun <varun1309@bitbucket.org>
2017-11-10 19:06:55 -0700

    Removed some unwanted get methods

--- app/weather.js
+++ app/weather.js
@@ -2,27 +2,6 @@ var express = require('express');
 var http = require('http');
 var router = express.Router();
 
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e0c683e83253bb4124ab2ac054a09591';
-
-router.get('/', function(req, res, next){
-
-	http.get(url, function(response){
-		var body = '';
-
-		response.on('data', function(chunk){
-			body = body+ chunk;
-		});
-
-		response.on('end', function(){
-			// var responseBody = JSON.parse(body);
-
-			res.render('index.html', {'response': body});
-		});
-	})
-
-});
-
-
 router.get('/fetch/:city', function(req, res, next){
 	var body = req.body;
 	var url = 'http://api.openweathermap.org/data/2.5/weather?q='+req.params.city+'+&appid=e0c683e83253bb4124ab2ac054a09591';
@@ -40,8 +19,6 @@ router.get('/fetch/:city', function(req, res, next){
 	})
 
 });
-
-
 
 
 module.exports = router;
